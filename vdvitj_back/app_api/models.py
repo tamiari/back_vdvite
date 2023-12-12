99f42cd4365ae04cf10d8060fed0d35d37f0d0d1
silvana-f <145504980+silvana-f@users.noreply.github.com>
2023-12-12 17:36:39 -0300

    Update models.py

--- vdvitj_back/app_api/models.py
+++ vdvitj_back/app_api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+#La clase Product hereda de la clase Model.
+class Product(models.Model):
+    
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    price = models.DateField(null=True)
+    #banner = models.ImageField(upload_to='imagenes/',null=True,verbose_name='Portada')
